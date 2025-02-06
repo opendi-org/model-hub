d53440080dd1c032bee125a502b0428396e82843
abmize <dirus.lupito@gmail.com>
2025-02-06 17:57:05 -0500

    Fixed typo in comment

--- api/main.go
+++ api/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
 	"opendi/model-hub/api/apiTypes"
 )
 
 func main() {
 	router := gin.Default()
-    router.GET("/models", getModels)
+	router.GET("/models", getModels)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
 
-// Example ednpoint that returns an empty CDM
+// Example endpoint that returns an empty CDM
 func getModels(c *gin.Context) {
 	var model = new(apiTypes.CausalDecisionModel)
-    c.IndentedJSON(http.StatusOK, model)
+	c.IndentedJSON(http.StatusOK, model)
 }
