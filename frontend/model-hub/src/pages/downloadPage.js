6ba954e5498a93859a6b8f2a890e6a5d2036791c
DirusLupito <dirus.lupito@gmail.com>
2025-02-07 14:44:25 -0500

    Added frontend code to make a get request in the backend.

--- frontend/model-hub/src/pages/downloadPage.js
+++ frontend/model-hub/src/pages/downloadPage.js
@@ -1,4 +1,31 @@
 import opendiIcon from '../opendi-icon.png';
+const cdm = {
+    creator : 'No CDM loaded'
+};
+
+async function getCDM() {
+    const url = "http://localhost:8080/models";
+    console.log("b");
+    try {
+      const response = await fetch(url,
+        {
+            method: 'GET'
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      console.log("a");
+  
+      const json = await response.json();
+      console.log(json);
+      cdm.creator = json.creator;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+
 const DownloadPage = () => {
     return (
         <div>
@@ -6,6 +33,8 @@ const DownloadPage = () => {
                 Click the button below to download a model from the OpenDI Model Hub
             </h1>
             <img src={opendiIcon} alt="OpenDI Icon" />
+            <button onClick={getCDM}>Get CDM</button>
+            <p>{cdm.creator}</p>
         </div>
     );
 };
