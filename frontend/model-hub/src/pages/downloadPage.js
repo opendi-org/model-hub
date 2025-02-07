fb3d551583bdd90dbedf3a9090e36177da4536ce
DirusLupito <dirus.lupito@gmail.com>
2025-02-07 12:55:13 -0500

    Added functionality in the frontend to route users to different webpages

---
+++ frontend/model-hub/src/pages/downloadPage.js
@@ -0,0 +1,13 @@
+import opendiIcon from '../opendi-icon.png';
+const DownloadPage = () => {
+    return (
+        <div>
+            <h1>
+                Click the button below to download a model from the OpenDI Model Hub
+            </h1>
+            <img src={opendiIcon} alt="OpenDI Icon" />
+        </div>
+    );
+};
+
+export default DownloadPage;
---
+++ frontend/model-hub/src/pages/index.js
@@ -0,0 +1,11 @@
+import opendiIcon from '../opendi-icon.png';
+const Home = () => {
+    return (
+        <div>
+            <h1>OpenDI Model Hub</h1>
+            <img src={opendiIcon} alt="OpenDI Icon" />
+        </div>
+    );
+};
+
+export default Home;
