eb456304a72b14740c3daaaca91993cd6ecbe56b
Jay Pham <jpham3@ncsu.edu>
2025-02-07 14:54:40 -0500

    IT1/styled-top-navigation-bar

--- frontend/model-hub/src/components/Navbar.js
+++ frontend/model-hub/src/components/Navbar.js
@@ -1,17 +1,37 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import opendiIcon from '../opendi-icon.png';
+
 const Navbar = () => {
     return (
-        <div>
-            <NavLink to="/UploadPage" activeStyle>
-                Upload models
-            </NavLink>
-            <NavLink to="/DownloadPage" activeStyle>
-                Download models
-            </NavLink>
-            <NavLink to="/" activeStyle>
-                Home
-            </NavLink>
-        </div>
+        <section class="navbar">
+            <div class="navbar_right">
+                <NavLink className="navbar_brand" to="/">
+                    <div class="navbar_logo">
+                        <img src={opendiIcon} alt="OpenDI Logo" />
+                    </div>
+                    <div class="navbar_title"> OpenDI </div>
+                </NavLink>
+
+                <form class="search_bar">
+                    <input type="text" value="Search OpenDI" />
+                </form>
+            </div>
+            <div class="navbar_option_items">
+                <nav class="nav_page_items">
+                    <NavLink to="" activeStyle>Search</NavLink>
+                    <NavLink to="/DownloadPage" activeStyle>Download</NavLink>
+                    <NavLink to="/UploadPage" activeStyle>Upload</NavLink>
+                    <NavLink to="" activeStyle>Popular</NavLink>
+                    <NavLink to="" activeStyle>About</NavLink>
+                </nav>
+
+                <div class="nav_account_items">
+                    <NavLink className="nav_login" to="" activeStyle>Login</NavLink>
+                    <NavLink className="nav_signup" to="" activeStyle>Sign Up</NavLink>
+                </div>
+            </div>
+        </section>
     );
 };
 
