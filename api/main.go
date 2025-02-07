d813f0e7dbd3b863ed1a035089c1edaa37c53777
DirusLupito <dirus.lupito@gmail.com>
2025-02-07 14:59:01 -0500

    Added additional model fields and access control header to allow frontend to call GET

--- api/main.go
+++ api/main.go
@@ -11,12 +11,18 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/models", getModels)
-
 	router.Run("localhost:8080")
 }
 
-// Example endpoint that returns an empty CDM
+// Example endpoint that returns an empty CDM, except for it's meta, which is provided with only a creator
 func getModels(c *gin.Context) {
 	var model = new(apiTypes.CausalDecisionModel)
+	model.ID = 1
+	model.Schema = "something"
+	model.MetaID = 1
+	var meta = new(apiTypes.Meta)
+	meta.Creator = "Lupito"
+	model.Meta = *meta
+	c.Header("Access-Control-Allow-Origin", "*")
 	c.IndentedJSON(http.StatusOK, model)
 }
