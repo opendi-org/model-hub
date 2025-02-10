d243da5ddcd887e252ef2a361f3ed3c4b7b24293
abmize <abmize@ncsu.edu>
2025-02-10 17:04:32 -0500

    Add environment variable integration checks for model hub address and port

--- api/main.go
+++ api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"opendi/model-hub/api/handlers" 
+	"opendi/model-hub/api/handlers"
+	"fmt"
+	"os"
 )
 
 func main() {
@@ -17,5 +19,24 @@ func main() {
 		models.GET("/", modelHandler.GetModels) // Use the handler method
 	}
 
-	router.Run("localhost:8080")
+	// Get the address and port from environment variables
+	// If not set, use default values
+	modelHubAddress := "localhost"
+	modelHubPort := "8080"
+	// Check to make sure the environment variables are set before using them
+	val, ok := os.LookupEnv("MODEL_HUB_ADDRESS")
+	if !ok || val == "" {
+		// error exit since the value is empty
+		fmt.Println("MODEL_HUB_ADDRESS is not set or empty")
+		os.Exit(1)
+	}
+	val, ok = os.LookupEnv("MODEL_HUB_PORT")
+	if !ok || val == "" {
+		// error exit since the value is empty
+		fmt.Println("MODEL_HUB_PORT is not set or empty")
+		os.Exit(1)
+	}
+		
+
+	router.Run(modelHubAddress + ":" + modelHubPort)
 }
