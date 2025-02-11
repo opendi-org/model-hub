dd65a684ac24ab87754b1b10a6ec0721454a81a8
esjun <esjun@ncsu.edu>
2025-02-10 20:51:33 -0500

    deleted app.css

--- frontend/model-hub/src/App.js
+++ frontend/model-hub/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,13 +12,13 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <Router>
-    <Navbar />
-    <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/uploadpage" element={<UploadPage />} />
-        <Route path="/downloadpage" element={<DownloadPage />} />
-    </Routes>
-</Router>
+      <Navbar />
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/uploadpage" element={<UploadPage />} />
+          <Route path="/downloadpage" element={<DownloadPage />} />
+      </Routes>
+    </Router>
   );
 }
 
