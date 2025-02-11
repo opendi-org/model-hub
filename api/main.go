0bab47c069492e1d1cf27225d51d973e024d8ee1
DirusLupito <dirus.lupito@gmail.com>
2025-02-10 20:26:30 -0500

    Updated reference to environment variables

--- api/main.go
+++ api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"opendi/model-hub/api/handlers"
 	"fmt"
+	"opendi/model-hub/api/handlers"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -20,23 +21,23 @@ func main() {
 	}
 
 	// Get the address and port from environment variables
-	// If not set, use default values
 	modelHubAddress := "localhost"
 	modelHubPort := "8080"
 	// Check to make sure the environment variables are set before using them
-	val, ok := os.LookupEnv("MODEL_HUB_ADDRESS")
+	val, ok := os.LookupEnv("OPENDI_MODEL_HUB_ADDRESS")
 	if !ok || val == "" {
 		// error exit since the value is empty
-		fmt.Println("MODEL_HUB_ADDRESS is not set or empty")
+		fmt.Println("Environment variable OPENDI_MODEL_HUB_ADDRESS is not set or empty")
 		os.Exit(1)
 	}
-	val, ok = os.LookupEnv("MODEL_HUB_PORT")
+	modelHubAddress = val
+	val, ok = os.LookupEnv("OPENDI_MODEL_HUB_PORT")
 	if !ok || val == "" {
 		// error exit since the value is empty
-		fmt.Println("MODEL_HUB_PORT is not set or empty")
+		fmt.Println("Environment variable OPENDI_MODEL_HUB_PORT is not set or empty")
 		os.Exit(1)
 	}
-		
+	modelHubPort = val
 
 	router.Run(modelHubAddress + ":" + modelHubPort)
 }
