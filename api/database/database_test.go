a1977d2ebdfaaabe90aa846cfff330f4a8b19cdf
Connorblu <cablumsack@gmail.com>
2025-03-27 23:57:46 -0400

    Adding 2 more unit tests in database.go

--- api/database/database_test.go
+++ api/database/database_test.go
@@ -293,7 +293,6 @@ func TestCreateModelGivenEmail(t *testing.T) {
 		t.Fatalf("Unable to create test user.")
 	}
 
-
 	meta := apiTypes.Meta{
 		ID:            30,
 		CreatedAt:     time.Now(),
@@ -325,12 +324,102 @@ func TestCreateModelGivenEmail(t *testing.T) {
 		t.Fatalf("There was an error when creating the model given the email. Status: %d Error:%s", status, err.Error())
 	}
 
-	status2, models, err := GetAllModels()
+	status2, models, _ := GetAllModels()
 	if status2 != http.StatusOK {
 		t.Fatalf("Get all models failed.")
 	}
 
 	if models[0].Meta.Name != "Email Test Model" {
-		t.Fatalf("The model was created successfully but the names do not match. \n Expected name: Email Test Model. \n Actual name: %s ", models[0].Meta.Name )
+		t.Fatalf("The model was created successfully but the names do not match. \n Expected name: Email Test Model. \n Actual name: %s ", models[0].Meta.Name)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	//IMPORTANT NOTE: In the current implementation, the user's email and username are the same!
+	//If/when this is eventually changed, this test must be edited! For now tests are written on the assumption
+	//that email and username are the same.
+	ResetTables()
+
+	user1, err1 := CreateUser("user1", "pass1")
+
+	if err1 != nil {
+		print(err1.Error())
+	}
+
+	if user1.Email != "user1" || user1.Username != "user1" || user1.Password != "pass1" {
+		t.Fatalf("Username or password is not set correctly")
+	}
+
+	status1, user1_copy, err1_1 := GetUserByEmail(user1.Email)
+	if status1 != http.StatusOK || err1_1 != nil {
+		t.Fatalf("Error when looking up user by email for user1")
+	}
+
+	if !user1.Equals(*user1_copy) {
+		t.Fatalf("No error was thrown when getting user1 by email, but the user retrieved does not match the one created.")
+	}
+
+	//Now check that we can create more users without conflict
+	user2, err2 := CreateUser("user2", "pass2")
+	if err2 != nil {
+		print(err2.Error())
+	}
+
+	if user2.Email != "user2" || user2.Username != "user2" || user2.Password != "pass2" {
+		t.Fatalf("Username or password is not set correctly")
+	}
+
+	status2, user2_copy, err2_2 := GetUserByEmail(user2.Email)
+	if status2 != http.StatusOK || err2_2 != nil {
+		t.Fatalf("Error when looking up user by email for user1")
+	}
+
+	if !user2.Equals(*user2_copy) {
+		t.Fatalf("No error was thrown when getting user1 by email, but the user retrieved does not match the one created.")
+	}
+
+	//Ensure UUIDs are NOT equal
+	if user1.UUID == user2.UUID {
+		t.Fatalf("User 1's UUID is the same as User 2's - this is extremely unlikely and almost certainly due to a bug.")
+	}
+
+	//Ensure we haven't regressed with User 1
+	status1, user1_copy, err1_1 = GetUserByEmail(user1.Email)
+	if status1 != http.StatusOK || err1_1 != nil {
+		t.Fatalf("Error when looking up user by email for user1")
+	}
+
+	if !user1.Equals(*user1_copy) {
+		t.Fatalf("No error was thrown when getting user1 by email, but the user retrieved does not match the one created.")
+	}
+}
+
+func TestUserLogin(t *testing.T) {
+	//As with TestCreateUser, it is important to note that this test was written with the assumption that
+	//the username and email are the same. If/when this is changed, make sure to edit this test!
+	ResetTables()
+
+	//Let's first login with a user that has not been created yet, and check that the user is properly created
+	status1, user1, err1 := UserLogin("email1", "pass1")
+	if status1 != http.StatusOK || err1 != nil {
+		t.Fatalf("Error was thrown when trying to login a brand new user")
+	}
+
+	//Now let's check that the user was actually created
+	status1_1, user1_copy, err1_1 := GetUserByEmail("email1")
+	if status1_1 != http.StatusOK || err1_1 != nil {
+		t.Fatalf("Error when trying to retrieve new user: %s", err1_1.Error())
+	}
+
+	if user1_copy.UUID != user1.UUID {
+		t.Fatalf("UUID's do not match between user object retrieved upon login, and user lookup by email")
+	}
+
+	//Now we can try and login again, but with a wrong email
+	status2, _, _ := UserLogin("email1", "wrong_password")
+	if status2 == http.StatusConflict {
+		t.Fatal("Trying to login with the wrong password throws an error that the user does not exist or there was some kind of database conflict.")
+	} else if status2 != http.StatusUnauthorized {
+		t.Fatal("User was able to login with the wrong password.")
 	}
 }
